20b2959653d722657fa2ea1966ee9fac090f01a2
olabudnik <olyabudnik98@gmail.com>
2022-08-28 18:21:38 +0200

    fixed API key

--- src/index.js
+++ src/index.js
@@ -127,10 +127,9 @@ forecastElement.innerHTML = forecastHTML;
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "62ba54e8cfba74fb731ce2be029cb2c";
+  let apiKey = "462ba54e8cfba74fb731ce2be029cb2c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
-  console.log(apiUrl);
-  axios.get(apiUrl).then(displayForecast);
+
 }
  
